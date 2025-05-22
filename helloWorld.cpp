cec59b0a02a782cb418fc70144e87a430dae37ac
Aditya Raj <rajaditya0024@gmail.com>
2025-05-22 18:15:58 +0530

    basic program

---
+++ helloWorld.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+
+/*
+
+Basic compilation: g++ my_program.cpp
+Specify output file name: g++ my_program.cpp -o my_program
+Run the program: my_program.exe
+
+*/
+
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
