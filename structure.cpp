3d9785563610319d818636712640c1199fe32109
Aditya Raj <rajaditya0024@gmail.com>
2025-05-22 19:32:08 +0530

    struct final

--- structure.cpp
+++ structure.cpp
@@ -14,6 +14,26 @@ book b4;
 
 // Global Declaration of variable of data structure book i.e a user defined data structure
 
+book input(){
+    book b0;
+    float price;
+    cout<<"Enter the id:";
+    cin>>b0.book_id;
+    cout<<"Enter the name";
+    cin>>b0.book_name;
+    cout<<"Enter the price";
+    cin>>b0.price;
+    return b0;
+}
+
+void display(book b){
+    cout<<endl;
+    cout<<"Book Id:"<<b.book_id<<"\t";
+    cout<<"Book Name:"<<b.book_name<<"\t";
+    cout<<"Price:"<<b.price<<"\t";
+    cout<<endl;
+    return;
+}
 
 
 int main(){
@@ -22,12 +42,13 @@ int main(){
 
     // Structure is use to store dissimilar values together.
 
-    cout<<"Structure";
+    cout<<"Structure"<<endl;
 
     // writing struct is optional
 
     struct book b1={ 1,"Nairobi", 2500 };
     // Local Declaration
+    cout<<b1.book_name<<endl;
     book b2;
     b2.book_id=2;
     b2.book_name="Tokyo";
@@ -38,6 +59,12 @@ int main(){
         float price;
     };
     //
+    b3=b2;
+    b4=input();
+    display(b1);
+    display(b2);
+    display(b3);
+    display(b4);
 
     return 0;
 }
