01c29a5771465d0d2c3723444e3144cb48221d1b
Aditya Raj <rajaditya0024@gmail.com>
2025-05-22 22:13:00 +0530

    oops with struct

--- structoops.cpp
+++ structoops.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 
 // Functions group mei hona chahiye
-struct Book{
+struct book{
     int id;
     string name;
     float price;
     void input(){
-
+        cout<<"Enter book id,name and price:";
+        cin>>id>>name>>price;
+    }
+    void display(book b){
+        cout<<endl<<b.id<<b.name<<b.price<<endl;
     }
 };
 using namespace std;
