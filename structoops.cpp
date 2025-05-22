a72c52a7ad1f6f62a0c9717e2dd003fab1bde381
Aditya Raj <rajaditya0024@gmail.com>
2025-05-22 22:34:18 +0530

    final struct

--- structoops.cpp
+++ structoops.cpp
@@ -2,21 +2,31 @@
 
 // Functions group mei hona chahiye
 struct book{
-    int id;
-    string name;
-    float price;
-    void input(){
-        cout<<"Enter book id,name and price:";
-        cin>>id>>name>>price;
-    }
-    void display(book b){
-        cout<<endl<<b.id<<b.name<<b.price<<endl;
-    }
+    // 3 variable
+    private:
+        int id;
+        string name;
+        float price;
+    // 2 functions
+    public:
+        void input(){
+            cout<<"Enter book id,name and price:";
+            cin>>id>>name>>price;
+            if(id<0)    id=-id;
+        }
+    void display(){
+    cout << endl << id << " " << name << " " << price << endl;
+}
+
 };
 using namespace std;
 
 int main(){
     cout<<"Object Oriented Programming using Structure"<<endl;
+    book b1;
+    b1.input();
+    b1.display();
+
 
     return 0;
 }
