aaaf5ff26b8441bf9528d73641f210659f4b28ab
Aditya Raj <rajaditya0024@gmail.com>
2025-05-22 22:09:06 +0530

    oops with struct

---
+++ structoops.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+// Functions group mei hona chahiye
+struct Book{
+    int id;
+    string name;
+    float price;
+    void input(){
+
+    }
+};
+using namespace std;
+
+int main(){
+    cout<<"Object Oriented Programming using Structure"<<endl;
+
+    return 0;
+}
